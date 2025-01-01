5166f113017cf1e7f45d8c113aa17a0c33ca5296
radosz365 <radosz365@gmail.com>
2025-01-01 16:07:05 +0100

    Updated create_grapf_from_csv and utilized it in the main script. Updated read_file_tests.py and renamed file.

--- read_file.py
+++ read_file.py
@@ -1,38 +1,41 @@
-import csv
+import pandas as pd
 import networkx as nx
-
+import os
+import csv
 
 class Node:
-    def __init__(self, course, lecture, group, classrom):
+    def __init__(self, course, lecturer, group, classrom):
         self.course = course
-        self.lecture = lecture
+        self.lecturer = lecturer
         self.group = group
         self.classrom = classrom
 
     def __str__(self):
-        return f"{self.course} {self.lecture} {self.group} {self.classrom}"
-
+        return f"{self.course} {self.lecturer} {self.group} {self.classrom}"
 
 def create_grapf_from_csv(filename):
     G = nx.Graph()
-    file_path = "datasets/" + filename
+    file_path = os.path.join("datasets", filename)
 
     try:
-        with open(file_path, "r", encoding="utf-8") as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                course = row[0]
-                lecture = row[1]
-                group = row[2]
-                classrom = row[3]
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"The file does not exist.")
 
-                node = Node(course, lecture, group, classrom)
-                G.add_node(node)
+        df = pd.read_csv(file_path)
 
-    except FileNotFoundError:
-        print("Plik CSV nie został znaleziony.")
-    except csv.Error as e:
-        print(f"Błąd podczas czytania pliku CSV: {e}")
-    except Exception as e:
-        print(f"Wystąpił nieoczekiwany błąd: {e}")
-    return G
+        if df.empty:
+            raise ValueError(f"The file is empty.")
+
+        required_columns = {"course", "lecturer", "group", "classroom"}
+
+        if not required_columns.issubset(df.columns):
+            raise ValueError(f"Invalid CSV format. Missing required columns.")
+
+        for index, row in df.iterrows():
+            node = Node(row["course"], row["lecturer"], row["group"], row["classroom"])
+            G.add_node(index, data=node)
+
+    except pd.errors.ParserError as e:
+        raise csv.Error(f"Error while parsing the CSV file: {e}")
+
+    return G, df
