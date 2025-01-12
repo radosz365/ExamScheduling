8899ffd00f9db0d6176d8082edc12652324ec65a
IamPawel <150140921+IamPawel@users.noreply.github.com>
2025-01-12 22:37:03 +0100

    add html files

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, static_folder="visualizations")
 
 @app.route("/home", methods=["GET"])
 def hello():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/show_schedule", methods=["GET"])
